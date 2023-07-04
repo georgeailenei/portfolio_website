42a85bae0f9f22f54e35f321ffbb7b2d4b6b2c10
georgeailenei <george.ailenei@gmail.com>
2023-07-04 17:44:36 +0100

    buttons added

---
+++ Portfolio/src/components/header/Buttons.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Buttons = () => {
+  return (
+    <div>
+      <a href="" className="btn btn-secondary">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-outline-success">
+        Contact me
+      </a>
+    </div>
+  );
+};
+  
--- Portfolio/src/components/header/Header.tsx
+++ Portfolio/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./header.css";
+import { Buttons } from "./Buttons";
 
 export const Header = () => {
   return (
@@ -8,6 +9,7 @@ export const Header = () => {
         <h6> Hi, I am</h6>
         <h1> George </h1>
         <h6> a software developer</h6>
+        <Buttons></Buttons>
       </div>
     </header>
   );
