1de0805d618fee089f0cc75443d598eae0b0bb41
georgeailenei <george.ailenei@gmail.com>
2023-07-04 18:04:29 +0100

    social media icons added

--- Portfolio/src/components/header/Header.tsx
+++ Portfolio/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.css";
 import { Buttons } from "./Buttons";
+import { Socials } from "./Socials";
 
 export const Header = () => {
   return (
@@ -9,7 +10,8 @@ export const Header = () => {
         <h6> Hi, I am</h6>
         <h1> George </h1>
         <h6> a software developer</h6>
-        <Buttons></Buttons>
+        <Buttons />
+        <Socials />
       </div>
     </header>
   );
---
+++ Portfolio/src/components/header/Socials.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+
+export const Socials = () => {
+  return (
+    <div>
+      <a href="https://linkedin.com/in/georgeailenei1992" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/georgeailenei" target="_blank">
+        <BsGithub />
+      </a>
+    </div>
+  );
+};
