57bd8da48eb6f54be1f572677d2d26e9a6e2bea5
georgeailenei <george.ailenei@gmail.com>
2023-07-06 21:32:31 +0100

    navigation bar done

--- Portfolio/src/components/nav/Navigation.tsx
+++ Portfolio/src/components/nav/Navigation.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+
 import "./nav.css";
 import { SiGooglehome } from "react-icons/si";
 import { MdDesignServices } from "react-icons/md";
@@ -7,21 +9,43 @@ import { MdContactSupport } from "react-icons/md";
 import { MdWorkHistory } from "react-icons/md";
 
 export const Navigation = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <SiGooglehome />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <MdDesignServices />
       </a>
-      <a href="#work">
+      <a
+        href="#work"
+        onClick={() => setActiveNav("#work")}
+        className={activeNav === "#work" ? "active" : ""}
+      >
         <MdWorkHistory />
       </a>
-      <a href="#references">
+      <a
+        href="#references"
+        onClick={() => setActiveNav("#references")}
+        className={activeNav === "#references" ? "active" : ""}
+      >
         <VscReferences />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <MdContactSupport />
       </a>
     </nav>
