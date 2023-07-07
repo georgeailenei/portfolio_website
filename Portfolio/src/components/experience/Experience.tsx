3b779f0686eb71956bc79e8955d6d20de39360c3
georgeailenei <george.ailenei@gmail.com>
2023-07-07 21:03:03 +0100

    icons added and some styling

--- Portfolio/src/components/experience/Experience.tsx
+++ Portfolio/src/components/experience/Experience.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./experience.css";
-import { BsPatchCheckFill } from "react-icons/bs";
 import { SiJavascript } from "react-icons/si";
 import { DiHtml5 } from "react-icons/di";
 import { SiCss3 } from "react-icons/si";
@@ -8,7 +7,7 @@ import { FaBootstrap } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
 import { FaVuejs } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
-import { DiDjango } from "react-icons/di";
+import { SiDjango } from "react-icons/si";
 import { TbSql } from "react-icons/tb";
 
 export const Experience = () => {
@@ -18,7 +17,6 @@ export const Experience = () => {
       <div className="container experience__container">
         <div className="experience__frontend">
           <h6>Frontend Development</h6>
-          <BsPatchCheckFill />
           <div className="experience__content">
             <article className="experience_details">
               <DiHtml5 />
@@ -43,13 +41,12 @@ export const Experience = () => {
 
         <div className="experience__backend">
           <h6>Backend Development</h6>
-          <BsPatchCheckFill />
           <div className="experience__content">
             <article className="experience_details">
               <FaPython />
             </article>
             <article className="experience_details">
-              <DiDjango />
+              <SiDjango />
             </article>
             <article className="experience_details">
               <TbSql />
