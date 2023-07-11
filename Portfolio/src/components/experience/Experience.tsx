1cea426de9cc00973ed5dd1a912f6ee549a7d5d7
georgeailenei <george.ailenei@gmail.com>
2023-07-11 18:27:10 +0100

    Icons aligned

--- Portfolio/src/components/experience/Experience.tsx
+++ Portfolio/src/components/experience/Experience.tsx
@@ -13,45 +13,45 @@ import { TbSql } from "react-icons/tb";
 export const Experience = () => {
   return (
     <section id="experience">
-      <h1>Experience</h1>
+      <h1>Skills</h1>
       <div className="container experience__container">
-        <div className="experience__frontend">
-          <h6>Frontend Development</h6>
-          <div className="experience__content">
-            <article className="experience_details">
-              <DiHtml5 />
-            </article>
-            <article className="experience_details">
-              <SiCss3 />
-            </article>
-            <article className="experience_details">
-              <FaBootstrap />
-            </article>
-            <article className="experience_details">
-              <SiJavascript />
-            </article>
-            <article className="experience_details">
-              <FaReact />
-            </article>
-            <article className="experience_details">
-              <FaVuejs />
-            </article>
-          </div>
-        </div>
-
-        <div className="experience__backend">
-          <h6>Backend Development</h6>
-          <div className="experience__content">
-            <article className="experience_details">
-              <FaPython />
-            </article>
-            <article className="experience_details">
-              <SiDjango />
-            </article>
-            <article className="experience_details">
-              <TbSql />
-            </article>
-          </div>
+        <div className="experience__content">
+          <article className="experience_details">
+            <DiHtml5 />
+            <h6>HTML</h6>
+          </article>
+          <article className="experience_details">
+            <SiCss3 />
+            <h6>CSS</h6>
+          </article>
+          <article className="experience_details">
+            <FaBootstrap />
+            <h6>Bootstrap</h6>
+          </article>
+          <article className="experience_details">
+            <SiJavascript />
+            <h6>JavaScript</h6>
+          </article>
+          <article className="experience_details">
+            <FaReact />
+            <h6>React</h6>
+          </article>
+          <article className="experience_details">
+            <FaVuejs />
+            <h6>Vue.js</h6>
+          </article>
+          <article className="experience_details">
+            <FaPython />
+            <h6>Python</h6>
+          </article>
+          <article className="experience_details">
+            <SiDjango />
+            <h6>Django</h6>
+          </article>
+          <article className="experience_details">
+            <TbSql />
+            <h6>SQL</h6>
+          </article>
         </div>
       </div>
     </section>
