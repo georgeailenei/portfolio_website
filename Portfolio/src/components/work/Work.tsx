8cba2f7c3e529944261cb1e2b50139fdf6d20ccc
georgeailenei <george.ailenei@gmail.com>
2023-07-17 14:32:55 +0100

    previous work component in progress

--- Portfolio/src/components/work/Work.tsx
+++ Portfolio/src/components/work/Work.tsx
@@ -1,6 +1,53 @@
 import React from "react";
 import "./work.css";
+import portfolio_item_01 from "../../assets/images/portfolio_item_01.jpg";
 
 export const Work = () => {
-  return <section id="work">Work</section>;
+  return (
+    <section id="work">
+      <h6>Recent Work</h6>
+      <h2>Portfolio</h2>
+
+      <div className="portfolio__container">
+        <article className="portfolio__item">
+          <img src={portfolio_item_01} className="porfolio__image" />
+          <h6>Portfolio Item</h6>
+          <div className="buttons">
+            <a href="#github" className="btn btn-outline-secondary btn-sm">
+              GitHub
+            </a>
+            <a href="#live" className="btn btn-outline-success btn-sm">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <img src={portfolio_item_01} className="porfolio__image" />
+          <h6>Portfolio Item</h6>
+          <div className="buttons">
+            <a href="#github" className="btn btn-outline-secondary btn-sm">
+              GitHub
+            </a>
+            <a href="#live" className="btn btn-outline-success btn-sm">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <img src={portfolio_item_01} className="porfolio__image" />
+          <h6>Portfolio Item</h6>
+          <div className="buttons">
+            <a href="#github" className="btn btn-outline-secondary btn-sm">
+              GitHub
+            </a>
+            <a href="#live" className="btn btn-outline-success btn-sm">
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
