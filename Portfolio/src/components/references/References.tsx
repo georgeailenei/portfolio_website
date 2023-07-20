200f0a24724805a6e6fdd56a62a106809f48ef5e
georgeailenei <george.ailenei@gmail.com>
2023-07-20 21:47:59 +0100

    references component build but grid must be used not flex

--- Portfolio/src/components/references/References.tsx
+++ Portfolio/src/components/references/References.tsx
@@ -1,6 +1,61 @@
 import React from "react";
 import "./references.css";
+import profile_picture from "../../assets/images/profile_photo.jpg";
 
 export const References = () => {
-  return <section id="references">References</section>;
+  return (
+    <section id="references">
+      <h2>References</h2>
+
+      <div className="container container__references">
+        <div className="reference__profile">
+          <div className="reference__header">
+            <img src={profile_picture} className="reference__img" />
+            <div className="reference__details">
+              <h6>
+                <strong>George Ailenei</strong>
+              </h6>
+              <h6>Software Developer</h6>
+            </div>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+
+        <div className="reference__profile">
+          <div className="reference__header">
+            <img src={profile_picture} className="reference__img" />
+            <div className="reference__details">
+              <h6>
+                <strong>George Ailenei</strong>
+              </h6>
+              <h6>Software Developer</h6>
+            </div>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.{" "}
+          </p>
+        </div>
+
+        <div className="reference__profile">
+          <div className="reference__header">
+            <img src={profile_picture} className="reference__img" />
+            <div className="reference__details">
+              <h6>
+                <strong>George Ailenei</strong>
+              </h6>
+              <h6>Software Developer</h6>
+            </div>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.{" "}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
