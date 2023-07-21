7e341dfe20b516d0d1641cf1bebafdea11240a7e
georgeailenei <george.ailenei@gmail.com>
2023-07-21 20:58:06 +0100

    contact component styling

--- Portfolio/src/components/contact/Contact.tsx
+++ Portfolio/src/components/contact/Contact.tsx
@@ -1,37 +1,45 @@
 import React from "react";
 import "./contact.css";
+import contact from "../../assets/images/contact.jpg";
 
 export const Contact = () => {
   return (
     <section id="contact">
       <h2>Contact</h2>
       <div className="container container__contact">
-        <img />
-        <div>
-          <form>
-            <input type="text" name="name" placeholder="Full Name" required />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email Address"
-              required
-            />
-            <input
-              type="text"
-              name="phone number"
-              placeholder="Phone Number"
-              required
-            />
-            <textarea
-              name="message"
-              placeholder="Your Message"
-              required
-            ></textarea>
-            <button type="submit" className="btn btn-success">
-              Send
-            </button>
-          </form>
-        </div>
+        <img src={contact} className="contact__img" />
+        <form>
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            placeholder="Full Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            placeholder="Email Address"
+            required
+          />
+          <input
+            type="text"
+            name="phone number"
+            className="form-control"
+            placeholder="Phone Number"
+            required
+          />
+          <textarea
+            name="message"
+            className="form-control"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-success">
+            Send
+          </button>
+        </form>
       </div>
     </section>
   );
