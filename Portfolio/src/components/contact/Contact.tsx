725f63464f6b443bb1babafac0b565e33ab8a3d9
georgeailenei <george.ailenei@gmail.com>
2023-07-21 20:30:39 +0100

    contact component created HTML

--- Portfolio/src/components/contact/Contact.tsx
+++ Portfolio/src/components/contact/Contact.tsx
@@ -2,5 +2,37 @@ import React from "react";
 import "./contact.css";
 
 export const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <div className="container container__contact">
+        <img />
+        <div>
+          <form>
+            <input type="text" name="name" placeholder="Full Name" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              required
+            />
+            <input
+              type="text"
+              name="phone number"
+              placeholder="Phone Number"
+              required
+            />
+            <textarea
+              name="message"
+              placeholder="Your Message"
+              required
+            ></textarea>
+            <button type="submit" className="btn btn-success">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
