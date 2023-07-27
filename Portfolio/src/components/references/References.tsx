9f3c7f0ba28b575d239d0de52eb6616a824a867e
georgeailenei <george.ailenei@gmail.com>
2023-07-27 18:52:26 +0100

    swipper added but not finished

--- Portfolio/src/components/references/References.tsx
+++ Portfolio/src/components/references/References.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import "./references.css";
 import profile_picture from "../../assets/images/profile_photo.jpg";
+import {
+  Pagination,
+  Navigation,
+  Autoplay,
+  EffectCoverflow,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export const References = () => {
   return (
     <section id="references">
       <h2>References</h2>
 
-      <div className="container container__references">
-        <div className="reference__profile">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        modules={[EffectCoverflow]}
+        className="container container__references"
+      >
+        <SwiperSlide className="reference__profile">
           <div className="reference__header">
             <img src={profile_picture} className="reference__img" />
             <div className="reference__details">
@@ -22,9 +46,9 @@ export const References = () => {
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
           </p>
-        </div>
+        </SwiperSlide>
 
-        <div className="reference__profile">
+        <SwiperSlide className="reference__profile">
           <div className="reference__header">
             <img src={profile_picture} className="reference__img" />
             <div className="reference__details">
@@ -38,9 +62,9 @@ export const References = () => {
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.{" "}
           </p>
-        </div>
+        </SwiperSlide>
 
-        <div className="reference__profile">
+        <SwiperSlide className="reference__profile">
           <div className="reference__header">
             <img src={profile_picture} className="reference__img" />
             <div className="reference__details">
@@ -54,8 +78,8 @@ export const References = () => {
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.{" "}
           </p>
-        </div>
-      </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
