e35cf1d24699046943dfbcad40481adf14d5c3ab
georgeailenei <george.ailenei@gmail.com>
2023-08-28 13:07:04 +0100

    changed navigation icons

--- Portfolio/src/components/nav/Navigation.tsx
+++ Portfolio/src/components/nav/Navigation.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { useState } from "react";
 
 import "./nav.css";
-import { SiGooglehome } from "react-icons/si";
-import { MdDesignServices } from "react-icons/md";
-import { VscReferences } from "react-icons/vsc";
-import { MdContactSupport } from "react-icons/md";
-import { MdWorkHistory } from "react-icons/md";
+import { AiFillAppstore } from "react-icons/ai";
+import { AiFillExperiment } from "react-icons/ai";
+import { AiFillMessage } from "react-icons/ai";
+import { AiFillFolderOpen } from "react-icons/ai";
 
 export const Navigation = () => {
   const [activeNav, setActiveNav] = useState("#");
@@ -18,35 +17,28 @@ export const Navigation = () => {
         onClick={() => setActiveNav("#")}
         className={activeNav === "#" ? "active" : ""}
       >
-        <SiGooglehome />
+        <AiFillAppstore />
       </a>
       <a
         href="#experience"
         onClick={() => setActiveNav("#experience")}
         className={activeNav === "#experience" ? "active" : ""}
       >
-        <MdDesignServices />
+        <AiFillExperiment />
       </a>
       <a
         href="#work"
         onClick={() => setActiveNav("#work")}
         className={activeNav === "#work" ? "active" : ""}
       >
-        <MdWorkHistory />
+        <AiFillFolderOpen />
       </a>
-      {/* <a
-        href="#references"
-        onClick={() => setActiveNav("#references")}
-        className={activeNav === "#references" ? "active" : ""}
-      >
-        <VscReferences />
-      </a> */}
       <a
         href="#contact"
         onClick={() => setActiveNav("#contact")}
         className={activeNav === "#contact" ? "active" : ""}
       >
-        <MdContactSupport />
+        <AiFillMessage />
       </a>
     </nav>
   );
